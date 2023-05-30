13de8fc35bbb8199b28650ffbf1462609635351c
Ali Sheikh <aiimiisheikh@gmail.com>
2023-05-29 22:13:22 -0400

    Made changes. Now shows highscore and resets snake.

--- snake.py
+++ snake.py
@@ -14,10 +14,18 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+        self.reset()
 
     def create_snake(self):
         for position in STARTING_POSITION:
             self.add_segment(position)
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def add_segment(self, position):
         sq = Turtle('square')
